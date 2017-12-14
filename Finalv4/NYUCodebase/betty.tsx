d29877f5414619d0afff139ceb1182bd021ef10b
janina3 <janina3@icloud.com>
2017-12-14 04:57:27 -0500

    Add files via upload

---
+++ Finalv4/NYUCodebase/betty.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="betty_0" tilewidth="21" tileheight="21" tilecount="81" columns="9">
+ <image source="betty_0.png" width="192" height="192"/>
+</tileset>
